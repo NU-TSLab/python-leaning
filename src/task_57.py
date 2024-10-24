f7c1c5df597e7519a7b3d11677acdab2912c14de
azumiyume <csym22001@g.nihon-u.ac.jp>
2024-10-25 00:41:59 +0900

    task_57:修正

--- src/task_57.py
+++ src/task_57.py
@@ -3,29 +3,19 @@ import itertools
 
 numbers=list(map(int, input().split()))
 print(f"入力された整数: {numbers}")#確認
-
-#numbers_P=list(itertools.permutations(numbers))
+#numbers_P=list(itertools.permutations(numbers))4P4
 #for p in numbers_P:
    # print (p) #確認
 
 enzanshi=['+', '-', '*', '/']
 enzanshi_P=list(itertools.product(enzanshi,repeat=3))#4P3
 
-#for nump in numbers:
 for enzP in enzanshi_P:
     join_P=f"{numbers[0]} {enzP[0]} {numbers[1]} {enzP[1]} {numbers[2]} {enzP[2]} {numbers[3]}"
         #print(join_P)
-
     try:
-
         result=eval(join_P)
         if result==10:
             print(f"{join_P}={result}")
     except ZeroDivisionError:
             continue
-
-
-
-
-
-
