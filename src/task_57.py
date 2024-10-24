94c83c7c0d5ed0f834f7dc8e3796124c29f2f75a
azumiyume <csym22001@g.nihon-u.ac.jp>
2024-10-25 00:28:27 +0900

    task_57:修正

--- src/task_57.py
+++ src/task_57.py
@@ -1,28 +1,27 @@
 import sys
 import itertools
 
-inputs = sys.argv[1:]#1こめ無視
-numbers = [int(i) for i in inputs]
+numbers=list(map(int, input().split()))
 print(f"入力された整数: {numbers}")#確認
 
-numbers_P=list(itertools.permutations(numbers))
+#numbers_P=list(itertools.permutations(numbers))
 #for p in numbers_P:
    # print (p) #確認
 
 enzanshi=['+', '-', '*', '/']
 enzanshi_P=list(itertools.product(enzanshi,repeat=3))#4P3
 
-for numP in numbers_P:
-    for enzP in enzanshi_P:
-        join_P=f"{numP[0]} {enzP[0]} {numP[1]} {enzP[1]} {numP[2]} {enzP[2]} {numP[3]}"
+#for nump in numbers:
+for enzP in enzanshi_P:
+    join_P=f"{numbers[0]} {enzP[0]} {numbers[1]} {enzP[1]} {numbers[2]} {enzP[2]} {numbers[3]}"
         #print(join_P)
 
-        try:
+    try:
 
-            result=eval(join_P)
-            if result==10:
-                print(f"{join_P}={result}")
-        except ZeroDivisionError:
+        result=eval(join_P)
+        if result==10:
+            print(f"{join_P}={result}")
+    except ZeroDivisionError:
             continue
 
 
