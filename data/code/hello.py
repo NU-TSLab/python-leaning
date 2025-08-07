6c2ba6d48313c28329460df76dfb871b1667df5e
KyoupuroAccount <csfu24087@g.nihon-u.ac.jp>
2025-08-07 11:52:52 +0900

    課題_42コードの更新

--- data/code/hello.py
+++ data/code/hello.py
@@ -4,3 +4,11 @@ class BankAccount:
 
     def deposit(self, amount):
         self.balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("残高不足")
+        else:
+            self.balance -= amount
+
+            
