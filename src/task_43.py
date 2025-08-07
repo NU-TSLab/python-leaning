05374a7e61c0f7dc62b8e77e42520371ba507cf9
KyoupuroAccount <csfu24087@g.nihon-u.ac.jp>
2025-08-07 15:06:33 +0900

    課題_43コードの更新

---
+++ src/task_43.py
@@ -0,0 +1,17 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+class Dog(Animal):
+    def speak(self):
+        return "ワンワン"
+    
+class Cat(Animal):
+    def speak(self):
+        return "ニャー"
+    
+dog = Dog("inu")
+cat = Cat("Neko")
+
+print(dog.speak())
+print(cat.speak())
