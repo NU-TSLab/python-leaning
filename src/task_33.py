a58048cb373d09ea35b27e94024d724071cf8732
TakumaSuetake <cstu24059@g.nihon-u.ac.jp>
2025-08-09 03:03:07 +0900

    task_33

---
+++ src/task_33.py
@@ -0,0 +1,6 @@
+fruit_dict = {'apple': 3, 'banana': 2, 'orange': 5}
+print(fruit_dict.get('banana'))
+fruit_dict['grape'] = 4
+fruit_dict['apple'] = 5
+print(fruit_dict.keys())
+print(fruit_dict.values())
