a385db7fe6fd325d498e26f2b84d22a580fe6329
kikuchi615 <csma24039@g.nihon-u.ac.jp>
2025-08-09 16:53:45 +0900

    6.データ形式(リスト、辞書、リスト内包形式)の回答

---
+++ src/task_31.py
@@ -0,0 +1,13 @@
+ls=[1,2,3,4,5]
+print(ls[0])
+print(ls[2])
+print(ls[-1])
+
+ls.append(6)
+print(ls)
+
+ls[2]=10
+print(ls)
+
+ls.insert(1,10)
+print(ls)
---
+++ src/task_33.py
@@ -0,0 +1,8 @@
+dict={'apple':3,'banana':2,'orange':5}
+print(dict['banana'])
+dict['grape']=4
+print(dict)
+dict['apple']=5
+print(dict)
+print(dict.keys())
+print(dict.values())
