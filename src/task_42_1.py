bb4ae7ad20b0822f7991d53cf07d5a85456c743d
datesann <cssa24070@g.nihon-u.ac.jp>
2025-08-10 19:10:23 +0900

    BankAccountクラス

---
+++ src/task_42_1.py
@@ -0,0 +1,16 @@
+class BankAccount:
+    def __init__(self):
+        self.balance=0
+    
+    def deposit(self,amount):
+        self.balance+=amount
+    
+    def withdraw(self,amount):
+        if amount>self.balance:
+            print("残高不足")
+        else:
+            self.balance-=amount
+
+account=BankAccount()
+account.deposit(50000)
+account.withdraw(70000)
