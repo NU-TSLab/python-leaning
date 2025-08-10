fd917bdc5451d83c1e5f9a8eea3dd174754af9c9
KyoupuroAccount <csfu24087@g.nihon-u.ac.jp>
2025-08-10 18:43:04 +0900

    課題_49のコードの更新

---
+++ src/task_49.py
@@ -0,0 +1,4 @@
+import task_49_module as listmodule
+
+number = [1, 2, 3, 4, 5]
+print(listmodule.sum_list(number))
---
+++ src/task_49_module.py
@@ -0,0 +1,7 @@
+# 49_module.py
+def sum_list(num):
+    p = 0
+    for i in range(len(num)):
+        p += num[i]
+
+    return p 
