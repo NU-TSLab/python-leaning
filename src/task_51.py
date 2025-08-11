cb86f8d7397159cf698dbc37c12f0004b5591cf7
datesann <cssa24070@g.nihon-u.ac.jp>
2025-08-11 18:57:20 +0900

    ファイル操作

---
+++ src/task_51.py
@@ -0,0 +1,9 @@
+import os
+
+with open('./data/sample.txt','w') as file:
+    file.write('Hello, World!')
+
+with open('./data/sample.txt','r') as file:
+    print(file.read())
+
+os.remove('./data/sample.txt')
