076be3e939c98989d967e1185332eac9cc685e83
datesann <cssa24070@g.nihon-u.ac.jp>
2025-08-11 19:06:25 +0900

    複数行ファイル書き込み

---
+++ src/task_52.py
@@ -0,0 +1,8 @@
+lines = ["Line 1","Line 2","Line 3","Line 4","Line 5"]
+
+with open('./data/multilines.txt','w') as file:
+    for line in lines:
+        file.write(line+'\n')
+
+with open('./data/multilines.txt','r') as file:
+    print(file.read())
