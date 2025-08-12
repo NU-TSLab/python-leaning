361ed6cc8b28223c95db9cb0b0bf2be33dd046e6
Melon285 <csso24004@g.nihon-u.ac.jp>
2025-08-12 19:49:58 +0900

    問41作成

---
+++ src/task_41.py
@@ -0,0 +1,8 @@
+class Car:
+    def __init__(self, make, year):
+        self.make = make
+        self.year = year
+
+    def car_info(self):
+        return f"{self.make}の車、年式: {self.year}"
+print(Car("toyota",2000).car_info())
