cca1bd7fbe134ca7f05be05f7f797d413153ca40
{yu-ki0702} <{csyi24093@g.nihon-u.ac.jp}>
2025-08-12 14:58:32 +0900

    問34

---
+++ src/task_34.py
@@ -0,0 +1,5 @@
+people=[{'name':'John','age':25},{'name':'Jane','age':30},{'name':'Doe','age':22}]
+ages=[person['age']for person in people]
+print(ages)
+name_over_25=[person['name']for person in people if person['age'] >= 25]
+print(name_over_25)
