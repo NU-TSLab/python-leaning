9cba8d04db715adcde92f8c432c3a501c9e20f91
{yu-ki0702} <{csyi24093@g.nihon-u.ac.jp}>
2025-08-13 01:24:51 +0900

    問41.py

---
+++ src/task_41.py
@@ -0,0 +1,8 @@
+class Car:
+    def __init__(self,make,year):
+        self.make=make
+        self.year=year
+    def car_info(self):
+        return f"{self.make}の車、年式:{self.year}"
+car=Car("Toyota",1990)
+print(car.car_info())
