b3e1e33e8b4fed202b9718fd1ed3fe45e648ba23
{yu-ki0702} <{csyi24093@g.nihon-u.ac.jp}>
2025-08-13 02:31:41 +0900

    問45

---
+++ src/task_45.py
@@ -0,0 +1,11 @@
+class Rectangle:
+    def __init__(self,width,height):
+        self.width=width
+        self.height=height
+    def area(self):
+        return self.width*self.height
+    def __str__(self):
+        return f"矩形　幅：{self.width},高さ：{self.height}"
+rect=Rectangle(5,6)
+print(rect)
+print(rect.area())
