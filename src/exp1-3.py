ba03e74d4d2cd0633872bcf908e7047cd453c5a8
KyoupuroAccount <csfu24087@g.nihon-u.ac.jp>
2025-08-18 21:24:51 +0900

    exp1-3のコードの更新 ・関数を用いないで実装

--- src/exp1-3.py
+++ src/exp1-3.py
@@ -4,8 +4,8 @@ import cv2
 import numpy as np
 import time
 
-WIDTH = 1920
-HEIGHT = 1080
+WIDTH = 800
+HEIGHT = 600
 FPS = 60
 
 cap = cv2.VideoCapture(0)
@@ -18,9 +18,17 @@ cap.set(cv2.CAP_PROP_FPS, FPS)
 ret, frame = cap.read()
 
 gray_lib = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+# 関数を使わない
+# 単純平均法(Avarage Method)
+gray_ave = np.mean(frame, axis=2).astype(np.uint8)
+
+# 最大値・最小値法(Desaturation)
+gray_desaturation = ((np.max(frame, axis=2) + np.min(frame, axis=2)) / 2).astype(np.uint8)
 
 cv2.imshow("Flame", frame)
 cv2.imshow("gray_1", gray_lib)
+cv2.imshow("gray_ave", gray_ave)
+cv2.imshow("gray_desaturation", gray_desaturation)
 
 cv2.waitKey(0)
 
