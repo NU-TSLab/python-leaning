39b9b65cc7fbaa172950bd0f07bb32247a964c11
TakumaSuetake <cstu24059@g.nihon-u.ac.jp>
2025-08-20 03:12:09 +0900

    task_47

---
+++ src/task_47.py
@@ -0,0 +1,5 @@
+import task_47_module
+
+print(task_47_module.greet("Takuma"))
+
+print(task_47_module.add(1, 2))
---
+++ src/task_47_module.py
@@ -0,0 +1,5 @@
+def greet(name):
+    return f"{name}さんこんにちは"
+
+def add(a, b):
+    return a + b
