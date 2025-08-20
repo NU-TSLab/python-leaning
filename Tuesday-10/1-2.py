9510486c1705315a7f502b60d5662ca6e89227a8
TakumaSuetake <cstu24059@g.nihon-u.ac.jp>
2025-08-21 04:55:54 +0900

    実験1 例題2

---
+++ Tuesday-10/1-2.py
@@ -0,0 +1,28 @@
+import os
+os.environ["OPENCV_VIDEOIO_MSMF_ENABLE_HW_TRANSFORMS"]="0"
+import cv2
+print(cv2.__version__)
+WIDTH = 1920
+HEIGHT = 1080
+FPS = 60
+
+cap = cv2.VideoCapture(0)
+
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
+cap.set(cv2.CAP_PROP_FPS, FPS)
+
+fps = cap.get(cv2.CAP_PROP_FPS)
+
+while (cap.isOpened()):
+
+    ret, frame = cap.read()
+
+    cv2.imshow("Frame", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
