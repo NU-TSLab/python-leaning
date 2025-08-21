9ab11fa49743ef626eed8bbe32cf329f292f2adf
TakumaSuetake <cstu24059@g.nihon-u.ac.jp>
2025-08-21 09:49:22 +0900

    実験1 例題2 改良版

--- Tuesday-10/1-2.py
+++ Tuesday-10/1-2.py
@@ -1,6 +1,7 @@
 import os
 os.environ["OPENCV_VIDEOIO_MSMF_ENABLE_HW_TRANSFORMS"]="0"
 import cv2
+import time
 print(cv2.__version__)
 WIDTH = 1920
 HEIGHT = 1080
@@ -14,15 +15,19 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
 cap.set(cv2.CAP_PROP_FPS, FPS)
 
 fps = cap.get(cv2.CAP_PROP_FPS)
-
+count = 0
+start = time.perf_counter()
 while (cap.isOpened()):
     ret = 0
     while not ret:
         ret, frame = cap.read()
     cv2.imshow("Frame", frame)
+    count += 1
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        end = time.perf_counter()
         break
 
 cap.release()
+print(f"実際のfps:{count / (end - start)}")
 cv2.destroyAllWindows()
