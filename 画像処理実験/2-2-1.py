d306e45bbb4818a2929fbcfcaae24cbe9e376518
Melon285 <csso24004@g.nihon-u.ac.jp>
2025-08-26 18:36:12 +0900

    フレーム間差分→背景差分に変更

--- 画像処理実験/2-2-1.py
+++ 画像処理実験/2-2-1.py
@@ -1,27 +1,34 @@
 import cv2
 import numpy as np
 
-# カメラを起動
 cap = cv2.VideoCapture(0)
 
-# フレーム取得
-ret, frame1 = cap.read()
+ret, background = cap.read()
 
 while True:
-    # 次のフレームを取得
-    ret, frame2 = cap.read()
+    ret, frame = cap.read()
 
-    # 差分計算
-    diff = cv2.absdiff(frame1, frame2)
+    #背景差分
+    diff = cv2.absdiff(background, frame)
 
-    # 表示
-    cv2.imshow("2", diff)
+    #グレースケール化
+    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
 
-    #更新
-    frame1 = frame2
+    #マスク
+    _, mask = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY)
+    mask_3ch = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+
+    #論理和
+    moving = cv2.bitwise_and(frame, mask_3ch)
+
+    #表示
+    cv2.imshow("moving", moving)
 
     if cv2.waitKey(30) == 27:
         break
 
 cap.release()
 cv2.destroyAllWindows()
+
+
+
