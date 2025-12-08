d1153bdb7caac1d8f417b80dd9abc6efd7a4671d
KyoupuroAccount <csfu24087@g.nihon-u.ac.jp>
2025-12-09 08:58:49 +0900

    変更2

--- src/exp1-2_ver2.0.py
+++ src/exp1-2_ver2.0.py
@@ -12,7 +12,7 @@ def decode_fourcc(v):
     v = int(v)
     return "".join([chr((v >> 8 * i) & 0xFF) for i in range(4)])
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
@@ -20,19 +20,24 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
 cap.set(cv2.CAP_PROP_FPS, FPS)
 
 frame_count = 0
-tmp = -1
-end_time = time.perf_counter()
-while (cap.isOpened()):
+start_time = time.perf_counter()
+
+while cap.isOpened():
     ret, frame = cap.read()
+    if not ret:
+        break
 
-    if tmp != int(frame):
-        frame_count += 1
+    frame_count += 1
 
-    cv2.imshow("Flame", frame)
+    cv2.imshow("Frame", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        print(end_time / frame_count)
         break
+
+end_time = time.perf_counter()
+elapsed = end_time - start_time
+print("平均フレーム時間:", elapsed / frame_count, "秒")
+print("実FPS:", frame_count / elapsed)
 
 cap.release()
 cv2.destroyAllWindows()
